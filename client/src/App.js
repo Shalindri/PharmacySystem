a24011ed68ef2b05f17f954f39315f09b1c346c8
Shalindri Perera <shalindri20@gmail.com>
2018-06-12 14:02:11 +0530

    client layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Landing from "./components/Layout/Landing";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar />
+
+        <Landing />
+
+        <Footer />
       </div>
     );
   }
