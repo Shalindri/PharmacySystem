ed1fae062c50e006bc1b7012eee60998bc85f3e0
Shalindri Perera <shalindri20@gmail.com>
2018-06-15 17:54:28 +0530

    integration1

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,16 @@ import React, { Component } from "react";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import Landing from "./components/Layout/Landing";
-import ViewDrugs from "./components/drug/ViewDrugs";
-import Sidebar from "./components/Layout/Sidebar";
+
 import "./App.css";
-import AddBatch from "./components/drug/AddBatch";
-import UpdateDrug from "./components/drug/UpdateDrug";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Sidebar />
 
-        <ViewDrugs />
+        <Landing />
 
         <Footer />
       </div>
