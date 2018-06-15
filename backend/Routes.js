160e3b4b2d135695cd7d155c2b577841c5defd06
Shalindri <36409448+Shalindri@users.noreply.github.com>
2018-06-15 17:22:04 +0530

    Update Routes.js

--- backend/Routes.js
+++ backend/Routes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 var routes = express.Router();
 
-var drugRoutes = require("./Routers/Drug.Route");
+var drugRoutes = require("./Routes/Drug.Route");
 var empRoutes = require("./Routers/Emp.Route");
 var issueRoutes = require("./Routers/Issue.Route");
 var paymentRoutes = require("./Routers/Payment.Route");
+var issuedDrugsRoutes = require("./Routes/IssuedDrugs.Route");
 
 routes.use("/drug", drugRoutes);
 routes.use("/emp", empRoutes);
 routes.use("/issue", issueRoutes);
 routes.use("/payment", paymentRoutes);
+routes.use("/issueddrugs", issuedDrugsRoutes);
 
 module.exports = routes;
