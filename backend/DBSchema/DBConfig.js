e5b082c6b4e00d68ed928e2189ccac343963105f
Shalindri <36409448+Shalindri@users.noreply.github.com>
2018-06-15 20:39:15 +0530

    Update DBConfig.js

--- backend/DBSchema/DBConfig.js
+++ backend/DBSchema/DBConfig.js
@@ -91,7 +91,6 @@ var Batch = new schema({
   }
 });
 
-
 var Order = new schema({
   Order_Id: {
     type: String,
@@ -112,39 +111,39 @@ var Order = new schema({
 });
 
 var Employee = new schema({
-  Emp_Id:{
-    type:String,
-    required:true
-},
-Type:{
-    type:String,
-    required:true
-},
-Emp_name:{
-    type:String,
-    required:true
-},
+  Emp_Id: {
+    type: String,
+    required: true
+  },
+  Type: {
+    type: String,
+    required: true
+  },
+  Emp_name: {
+    type: String,
+    required: true
+  },
 
-Email:{
-    type:String,
-    required:true
-},
-Password:{
-    type:String,
-    required:true
-},
-Address:{
-    type:String,
-    required:true
-},
-Telephone:{
-    type:String,
-    required:true
-},
-Date:{
-    type:String,
-    required:true
-}
+  Email: {
+    type: String,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  Address: {
+    type: String,
+    required: true
+  },
+  Telephone: {
+    type: String,
+    required: true
+  },
+  Date: {
+    type: String,
+    required: true
+  }
 });
 
 var Employee_salary = new schema({
@@ -185,85 +184,85 @@ var Employee_duty = new schema({
   }
 });
 
-var Prescription_data=new schema({
-  Pid:{
-      type:String,
-      required:true
+var Prescription_data = new schema({
+  Pid: {
+    type: String,
+    required: true
   },
-  Patient_name:{
-      type:String,
-      required:true
+  Patient_name: {
+    type: String,
+    required: true
   },
-  Pres_Id:{
-      type:String,
-      required:true
+  Pres_Id: {
+    type: String,
+    required: true
   },
-  Create_Date:{
-      type:String,
-      required:true
+  Create_Date: {
+    type: String,
+    required: true
   },
-  Drug_type1:{
-      type:String,
-      required:true
+  Drug_type1: {
+    type: String,
+    required: true
   },
-  Drug_type2:{
-      type:String,
-      required:true
+  Drug_type2: {
+    type: String,
+    required: true
   },
-  Dosage:{
-      type:String,
-      required:true
+  Dosage: {
+    type: String,
+    required: true
   },
-  Frequency:{
-      type:String,
-      required:true
+  Frequency: {
+    type: String,
+    required: true
   },
-  Period:{
-      type:String,
-      required:true
- }
+  Period: {
+    type: String,
+    required: true
+  }
 });
 
-var Issue_data=new schema({
-  Pres_Id:{
-      type:String,
-      required:true
+var Issue_data = new schema({
+  Pres_Id: {
+    type: String,
+    required: true
   },
-  Drug_name1:{
-      type:String,
-      required:true
+  Drug_name1: {
+    type: String,
+    required: true
   },
-  Drug_name2:{
-    type:String,
-    required:true
+  Drug_name2: {
+    type: String,
+    required: true
   },
-  Issue_Date:{
-      type:String,
-      required:true
+  Issue_Date: {
+    type: String,
+    required: true
   },
-  Quantity1:{
-      type:String,
-      required:true
+  Quantity1: {
+    type: String,
+    required: true
   },
-  Quantity2:{
-    type:String,
-    required:true
- }
+  Quantity2: {
+    type: String,
+    required: true
+  }
 });
 var Payment_data = new schema({
   BillNo: {
     type: String,
     required: true
   },
-  Pres_Id:{
+  Pres_Id: {
     type: String,
     required: true
   },
-  PayDate:{
+  PayDate: {
     type: String,
     required: true
   },
-  Total:{
+  Total: {
     type: String,
     required: true
   }
@@ -277,7 +276,6 @@ mongoose.model("duty", Employee_duty);
 mongoose.model("emp_salary", Employee_salary);
 mongoose.model("pres_data", Prescription_data);
 mongoose.model("issue_data", Issue_data);
-
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/pharmacy",
